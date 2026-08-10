d5f50eb87312f21439c6aa1cc772006a96a9fcbb
utkarshakashetwar-hub <utkarshakashetwar@gmail.com>
2026-08-10 14:12:50 +0530

    Add files via upload

---
+++ buy & shell.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxProfit = 0;
+        int bestBuy = prices[0];
+
+        for (int i = 0; i < prices.size(); i++) {
+            if (prices[i] > bestBuy) {
+                maxProfit = max(maxProfit, prices[i] - bestBuy);
+            }
+
+            bestBuy = min(bestBuy, prices[i]);
+        }
+
+        return maxProfit;
+    }
+};
